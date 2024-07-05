c0e102ebecef09c26cb494ed407fe0cc63539129
Rushikesh-Pandule <rushipandule50@gmail.com>
2024-07-05 19:52:16 +0530

    even odd programm

---
+++ evenODD.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+    int i=0;
+    printf("\n ENTER ANY NUMBER = ");
+    scanf("\n %d",&i);
+    if(i%2==0)
+    printf("\n NUMBER IS EVEN ");
+    else
+    printf("\n NUMBER IS ODD ");
+}
